5dc79afff9b35b1f9b6d5ba7f9efd70e5023dca6
Imrerom05 <imre.romstad@gmail.com>
2025-02-28 11:21:22 +0100

    added card sorts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,13 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
 
 public class Main extends Application implements EventHandler<ActionEvent> {
     DeckOfCards deck = new DeckOfCards();
@@ -16,19 +17,55 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 
     @Override
     public void start(Stage stage) {
+        // Set the title of the stage
         stage.setTitle("Playing Card App");
 
+        // Create a vertical box to hold the buttons
         VBox rightMenue = new VBox();
         DrawHand = new Button("Draw Hand");
         DrawHand.setOnAction(this);
+
+        DrawHand.setOnAction(e -> {
+            System.out.println("Drawing a hand of 5 cards: " + deck.dealHand(5));
+        });
+
         Shuffle = new Button("Shuffle Deck");
         Shuffle.setOnAction(this);
         rightMenue.getChildren().addAll(DrawHand, Shuffle);
 
+        // Create an image view for clubs
+        Image clubsImage = new Image(getClass().getResourceAsStream("/clubs.png"));     
+        ImageView clubsView = new ImageView(clubsImage);
+        clubsView.setFitHeight(30);
+        clubsView.setFitWidth(30);
+
+        // Create an image view for dimonds
+        Image dimondsImage = new Image(getClass().getResourceAsStream("/dimonds.png"));     
+        ImageView dimondsView = new ImageView(dimondsImage);
+        dimondsView.setFitHeight(30);
+        dimondsView.setFitWidth(30);
+
+        // Create an image view for hearts
+        Image heartsImage = new Image(getClass().getResourceAsStream("/hearts.png"));     
+        ImageView heartsView = new ImageView(heartsImage);
+        heartsView.setFitHeight(30);
+        heartsView.setFitWidth(30);
+
+        // Create an image view for spades
+        Image spadesImage = new Image(getClass().getResourceAsStream("/spades.png"));
+        ImageView spadesView = new ImageView(spadesImage);
+        spadesView.setFitHeight(30);
+        spadesView.setFitWidth(30);
+
+        HBox hand = new HBox();
+
+        // Create a border pane to hold the buttons
         BorderPane borderPane = new BorderPane();
         borderPane.setRight(rightMenue);
+        borderPane.setCenter(hand);
 
 
+        // Create a scene
         Scene scene = new Scene(borderPane, 400, 300);
         stage.setScene(scene);
         stage.show();
